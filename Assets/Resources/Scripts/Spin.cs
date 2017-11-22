e3eb7e7a659432132e32e6d731adec138833053f
pmartin36 <pmartin36@gmail.com>
2017-11-22 14:19:41 -0500

    metadata files

---
+++ Assets/Resources/Scripts/Spin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	ParticleSystem particleSystem;
+
+	// Use this for initialization
+	void Start () {
+		particleSystem = GetComponentInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3( -3 * Time.deltaTime, 0, 0);
+	}
+}
